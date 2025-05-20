b7ba7f47bccd650ad393a88b580ae059f76d9ea9
Andrei Bachinin <limbus727@gmail.com>
2025-05-19 22:11:53 -0600

    feat: films part added

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,44 @@
-//TODO описать DTO для запросов к /films
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class Schedule {
+  id: string;
+  daytime: string;
+  hall: number;
+  rows: number;
+  seats: number;
+  price: number;
+  taken: string[];
+}
+
+export class GetFilmDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  title: string;
+  about: string;
+  description: string;
+  image: string;
+  cover: string;
+  schedule: Schedule[];
+}
+
+export class ReturnFilms {
+  total: number;
+  items: GetFilmDto[];
+}
+
+export class ReturnSchedules {
+  total: number;
+  items: Schedule[];
+}
+
+export class FindFilmIdParams {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
+
+export class ReturnError {
+  error: string;
+}
---
+++ backend/src/films/films.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { FilmsRepository } from '../repository/films.repository';
+import { ReturnError, ReturnFilms, ReturnSchedules } from './dto/films.dto';
+
+@Injectable()
+export class FilmsService {
+  constructor(private readonly filmsRepository: FilmsRepository) {}
+
+  async findAll(): Promise<ReturnFilms> {
+    const result = await this.filmsRepository.findAll();
+    return { total: result.length, items: result };
+  }
+
+  async findScheduleById(id: string): Promise<ReturnSchedules | ReturnError> {
+    try {
+      const result = await this.filmsRepository.findFilmById(id);
+      if (!result) {
+        throw new Error('По заданному id данные не найдены');
+      }
+      return { total: result.schedule.length, items: result.schedule };
+    } catch (e) {
+      return { error: e.message };
+    }
+  }
+}
